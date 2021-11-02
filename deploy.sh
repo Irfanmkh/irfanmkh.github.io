5a8382c37d890aa6c8e569ccada4ff4f2b136c14
ThePhilosopher37 <75205458+ThePhilosopher37@users.noreply.github.com>
2021-11-03 01:29:47 +0530

    time

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
-# Build the project.
+# Build the project
 echo ""
 echo ""
 echo "Committing changes to $(pwd)"
@@ -19,3 +19,29 @@ echo "Committing changes to $(pwd)"
 git add .
 git commit -m "$msg"
 git push origin main
+
+printf "\033[0;32mDeployment complete.\033[0m\n"
+printf "\033[0;32mWe are ready for liftoff.\033[0m\n"
+
+# Yes.
+if [ "$1" = "time" ]; then
+	echo ""
+	echo ""
+	echo "\033[0;32mT-10...\033[0m"
+	sleep 2
+	echo "\033[0;32m8...\033[0m"
+	sleep 2
+	echo "\033[0;32m6...\033[0m"
+	sleep 2
+	echo "\033[0;32m4...\033[0m"
+	sleep 2
+	echo "\033[0;32m2...\033[0m"
+	sleep 2
+	echo "\033[0;32mWe have liftoff!\033[0m"
+	echo ""
+	echo ""
+	$1
+else
+	echo ""
+	echo ""
+fi
