69242cb247ed1c81076c6c1658f46cc78f364a91
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2026-09-14 10:33:25 +0700

    Update Hotjar ID in gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -285,7 +285,7 @@ const CONFIG = {
     },
   ],
   // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: { id: '', snippetVersion: 6 },
+  hotjar: { id: 'ff2ae38c83da6', snippetVersion: 6 },
   themeConfig: {
     defaultTheme: 'lofi',
 
